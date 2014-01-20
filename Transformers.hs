3789436f5891d6c939ada95b272224bfcca1d048
HounD <vladshikhov@gmail.com>
2014-01-20 18:12:03 +0400

    eval2b

--- Transformers.hs
+++ Transformers.hs
@@ -30,9 +30,11 @@ eval env (Lit i)       =   return $ IntVal i
 
 eval env (Var n)       =   return $ fromJust (Map.lookup n env)
 
-eval env (Plus e1 e2)  =   do  IntVal i1  <- eval env e1
-                               IntVal i2  <- eval env e2
-                              return $ IntVal (i1 + i2)
+eval env (Plus e1 e2)  =   do  e1'  <- eval env e1
+                               e2'  <- eval env e2
+                               case (e1', e2') of
+                                     (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                                     _ -> throwError "type error"
 
 eval env (Abs n e)     =   return $ FunVal env n e
 
@@ -40,12 +42,12 @@ eval env (App e1 e2)   =   do  val1  <- eval env e1
                                val2  <- eval env e2
                                case val1 of
                                      FunVal env' n body -> eval (Map.insert n val2 env') body
+                                     _ -> throwError "type error"
 
 
-exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
-
+--exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+exampleExp = Plus (Lit 1) (Abs "x" (Var "x"))
 
 main :: IO ()
 main = do
   print $ runEval (eval Map.empty exampleExp)
-
