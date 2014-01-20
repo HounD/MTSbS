3e72ff7ee2f4ccc79bb82c2cf498bb3e3418d3f2
HounD <vladshikhov@gmail.com>
2014-01-20 17:59:18 +0400

    eval2a

--- Transformers.hs
+++ Transformers.hs
@@ -1,5 +1,6 @@
 
 import Control.Monad.Identity
+import Control.Monad.Error
 
 import Data.Maybe
 import qualified Data.Map as Map
@@ -19,10 +20,10 @@ data Value  =  IntVal Integer
 
 type Env    =  Map.Map Name Value
 
-type Eval alpha  =   Identity alpha
+type Eval alpha  =   ErrorT String Identity alpha
 
-runEval          ::  Eval alpha -> alpha
-runEval ev       =   runIdentity ev
+runEval         :: Eval alpha -> Either String alpha
+runEval ev      = runIdentity (runErrorT ev)
 
 eval                   ::  Env -> Exp -> Eval Value
 eval env (Lit i)       =   return $ IntVal i
@@ -31,14 +32,14 @@ eval env (Var n)       =   return $ fromJust (Map.lookup n env)
 
 eval env (Plus e1 e2)  =   do  IntVal i1  <- eval env e1
                                IntVal i2  <- eval env e2
-                               return $ IntVal (i1 + i2)
+                              return $ IntVal (i1 + i2)
 
 eval env (Abs n e)     =   return $ FunVal env n e
 
 eval env (App e1 e2)   =   do  val1  <- eval env e1
                                val2  <- eval env e2
                                case val1 of
-                                   FunVal env' n body -> eval (Map.insert n val2 env') body
+                                     FunVal env' n body -> eval (Map.insert n val2 env') body
 
 
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
@@ -46,4 +47,5 @@ exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 
 main :: IO ()
 main = do
-	print $ runEval (eval Map.empty exampleExp)
+  print $ runEval (eval Map.empty exampleExp)
+
