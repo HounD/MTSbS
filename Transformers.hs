3c8a2b36e92bb6344ddbf370099b604c7ddf0ebd
HounD <vladshikhov@gmail.com>
2014-02-02 15:54:50 +0400

    eval2c

--- Transformers.hs
+++ Transformers.hs
@@ -25,24 +25,21 @@ type Eval alpha  =   ErrorT String Identity alpha
 runEval         :: Eval alpha -> Either String alpha
 runEval ev      = runIdentity (runErrorT ev)
 
-eval                   ::  Env -> Exp -> Eval Value
+eval                   ::  Env -> Exp -> Eval Value 
+
 eval env (Lit i)       =   return $ IntVal i
 
 eval env (Var n)       =   return $ fromJust (Map.lookup n env)
 
-eval env (Plus e1 e2)  =   do  e1'  <- eval env e1
-                               e2'  <- eval env e2
-                               case (e1', e2') of
-                                     (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
-                                     _ -> throwError "type error"
+eval env (Plus e1 e2)  =   do  IntVal i1  <- eval env e1
+                               IntVal i2  <- eval env e2
+                               return $ IntVal (i1 + i2)
 
 eval env (Abs n e)     =   return $ FunVal env n e
 
-eval env (App e1 e2)   =   do  val1  <- eval env e1
-                               val2  <- eval env e2
-                               case val1 of
-                                     FunVal env' n body -> eval (Map.insert n val2 env') body
-                                     _ -> throwError "type error"
+eval env (App e1 e2)   =   do  FunVal env' n body  <- eval env e1
+                               val2                <- eval env e2
+                               eval (Map.insert n val2 env') body                                    
 
 
 --exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
