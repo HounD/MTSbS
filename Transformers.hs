b2ab754993574b22dc67cd781c59f2c4fd3fd0c4
HounD <vladshikhov@gmail.com>
2014-02-02 21:21:43 +0400

    eval4 unfinished

--- Transformers.hs
+++ Transformers.hs
@@ -2,6 +2,7 @@
 import Control.Monad.Identity
 import Control.Monad.Error
 import Control.Monad.Reader
+import Control.Monad.State
 
 import Data.Maybe
 import qualified Data.Map as Map
@@ -21,40 +22,48 @@ data Value  =  IntVal Integer
 
 type Env    =  Map.Map Name Value
 
-type Eval alpha  =   ReaderT Env (ErrorT String Identity) alpha
+type Eval alpha = ReaderT Env (ErrorT String (StateT Integer Identity)) alpha
 
-runEval         :: Env -> Eval alpha -> Either String alpha
-runEval env ev  = runIdentity (runErrorT (runReaderT ev env))
- 
+runEval            ::  Env -> Integer -> Eval alpha -> (Either String alpha, Integer)
+runEval env st ev  =   runIdentity (runStateT (runErrorT (runReaderT ev env)) st)
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do st <- get
+          put (st + 1)
+
 eval                   ::  Exp -> Eval Value
 
-eval (Lit i)           =   return $ IntVal i
+eval (Lit i)          =   do  tick
+                              return $ IntVal i
 
-eval (Var n)           =   do  env <- ask
-                               case Map.lookup n env of
-                                    Nothing -> throwError ("unbound variable: " ++ n)
-                                    Just val -> return val
+eval (Var n)          =   do  tick
+                              env <- ask
+                              case Map.lookup n env of
+                                   Nothing -> throwError ("unbound variable: " ++ n)
+                                   Just val -> return val                                    
 
-eval (Plus e1 e2)      =   do  e1' <- eval e1
-                               e2' <- eval e2
-                               case (e1', e2') of
-                                    (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
-                                    _ -> throwError "type error in addition"
+eval (Plus e1 e2)     =   do  tick
+                              e1'  <- eval e1
+                              e2'  <- eval e2
+                              case (e1', e2') of
+                                   (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+                                   _ -> throwError "type error in addition"                                  
 
-eval (Abs n e)         =   do  env <- ask
-                               return $ FunVal env n e
+eval (Abs n e)        =   do  tick
+                              env <- ask
+                              return $ FunVal env n e                               
 
-eval  (App e1 e2)      =   do  val1 <- eval e1
-                               val2 <- eval e2
-                               case val1 of
-                                    FunVal env' n body -> local (const (Map.insert n val2 env')) (eval body)
-                                    _ -> throwError "type error in application"                                    
+eval (App e1 e2)      =   do  tick
+                              val1  <- eval e1
+                              val2  <- eval e2
+                              case val1 of
+                                   FunVal env' n body -> local (const (Map.insert n val2 env')) (eval body)
+                                   _ -> throwError "type error in application"                                                                        
 
 
---exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
 --exampleExp = Plus (Lit 1) (Abs "x" (Var "x"))
-exampleExp = Var "x"
-
+--exampleExp = Var "x"
 
 main :: IO ()
 main = do
